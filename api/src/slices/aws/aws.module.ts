db1889c31d40f5ac339aeb307e770c564c765e76
Dmitriy Zhuk <dmitriyzhuk@gmail.com>
2025-03-11 12:12:51 +0200

    fix: remove aws services

--- api/src/slices/aws/aws.module.ts
+++ api/src/slices/aws/aws.module.ts
@@ -5,15 +5,22 @@ import { Module } from '@nestjs/common';
 // import { ModelsService } from './domain/services';
 // import { ChatMapper, MessageMapper } from './data/mappers';
 // import { OpenAIRepository } from './data/repositories';
-import { DynamoModule } from './dynamo/dynamo.module';
-import { BedrockModule } from './bedrock/bedrock.module';
-import { LambdaModule } from './lambda/lambda.module';
-import { OpensearchModule } from './opensearch/opensearch.module';
+// import { DynamoModule } from './dynamo/dynamo.module';
+// import { BedrockModule } from './bedrock/bedrock.module';
+// import { LambdaModule } from './lambda/lambda.module';
+// import { OpensearchModule } from './opensearch/opensearch.module';
 import { S3Module } from './s3/s3.module';
 import { CognitoModule } from './cognito/cognito.module';
 
 @Module({
-  imports: [DynamoModule, BedrockModule, OpensearchModule, S3Module, LambdaModule, CognitoModule],
+  imports: [
+    // DynamoModule,
+    // BedrockModule,
+    // OpensearchModule,
+    S3Module,
+    // LambdaModule,
+    CognitoModule,
+  ],
   //   providers: [
   //     { provide: IModelsGateway, useClass: ModelsGateway },
   //     ModelsService,
@@ -21,6 +28,13 @@ import { CognitoModule } from './cognito/cognito.module';
   //     // MessageMapper,
   //   ],
   //   controllers: [ModelsController],
-  exports: [DynamoModule, BedrockModule, OpensearchModule, S3Module, LambdaModule, CognitoModule],
+  exports: [
+    // DynamoModule,
+    //  BedrockModule,
+    //  OpensearchModule,
+    S3Module,
+    // LambdaModule,
+    CognitoModule,
+  ],
 })
 export class AwsModule {}
